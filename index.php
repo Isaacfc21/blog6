79c7a188f5169f4d20e68b228b7d6cfddce77c06
Isaac Caraca <isaaccaracayahoo@gmai.com>
2025-07-25 16:51:28 -0300

    blog

--- index.php
+++ index.php
@@ -2,11 +2,20 @@
 
     require 'vendor/autoload.php';
     // require 'rotas.php'; 
+    use sistema\biblioteca\Upload;
+
+    $upload = new Upload("upload");
+    $upload->arquivo('imagens'); 
+    r($upload);
+    var_dump($upload);
+
+    if(!empty($arquivo = $_FILES)){
+        // r($arquivo);
+    }
 
     $arquivo = $_FILES;
-    r($arquivo);
     echo '<hr>';
-    echo $arquivo['arquivo']['name'];
+    // echo $arquivo['arquivo']['name'];
     // var_dump($arquivo);
     // print_r($arquivo);
 ?>  
---
+++ sistema/biblioteca/Upload.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace sistema\biblioteca;
+
+use Directory;
+
+    /**
+     * Classe Upload
+     * 
+     * @author Isaac Caraça <isaaccaracayahoo@gmail.com>
+     */
+
+class Upload
+{
+    public $diretorio;
+    public $arquivo;
+    public $nome;
+    public $pasta;
+
+    public function __construct(string $diretorio = null)
+    {
+        $this->diretorio = $diretorio ?? 'uploads';
+        if(!file_exists($this->diretorio) && !is_dir($this->diretorio)){
+            mkdir($this->diretorio, 0755);
+        }
+    }
+
+    public function arquivo(string $pasta = null)
+    {
+        $this->pasta = $pasta ?? 'arquivos';
+
+        $this->criarPasta();
+    }
+
+    public function criarPasta(): void
+    {
+        if (!file_exists($this->diretorio).DIRECTORY_SEPARATOR.$this->pasta && !is_dir($this->diretorio).DIRECTORY_SEPARATOR.$this->pasta) {
+            mkdir($this->diretorio.DIRECTORY_SEPARATOR.$this->pasta, 0755);
+        }
+    }
+
+}
+
+?>
